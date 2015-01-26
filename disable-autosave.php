5e349111ee1b84ac16d8e50900a4ef361def0009
Jens Ahrengot Boddum <ahrengot@gmail.com>
2015-01-26 14:18:51 +0100

    Initial commit

---
+++ disable-autosave.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ Plugin Name: Disable Autosave
+ Plugin URI: http://lopo.it/duplicate-post-plugin/
+ Description: Disabled the autosave feature in WordPress.
+ Version: 1.0
+ Author: Jens Ahrengot Boddum
+ Author URI: http://ahrengot.com/
+ */
+
+function a_deregister_autosave_script() {
+	if ( is_admin() ) {
+		wp_deregister_script( 'autosave' );
+	}
+}
+add_action( 'init', 'a_deregister_autosave_script' );
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Disable autosave plugin for WordPress
+
+### How it works
+Simply activate the plugin and the autosave script won't be loaded in the editor.
